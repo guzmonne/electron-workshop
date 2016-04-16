c9e8764e0f29f6f27853f5a0bd940f1171704cbb
Guzman Monne <guzmonne@hotmail.com>
2016-04-16 15:11:36 -0300

    finished third exercise

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ document.addEventListener( "DOMContentLoaded", function() {
     let version = process.version;
     let e = document.getElementById( "info" );
     e.textContent = "I'm running Node.js version: " + version;
+
+    let btn = document.getElementById( "clickme" );
+    btn.addEventListener( "click", function( e ) {
+        console.log( "I was clicked." );
+    } );
 } );
